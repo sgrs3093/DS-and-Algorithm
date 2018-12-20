f264b270bf84c7554a9e86af4ebf989c471f7f34
sgr <sgrs3093@gmail.com>
2018-12-20 15:37:15 +0530

    added the code of maxsubArray for all negative numbers using Math library in java

--- src/main/java/array/largestSumContigiousSubArray/KadaneAlgo.java
+++ src/main/java/array/largestSumContigiousSubArray/KadaneAlgo.java
@@ -21,6 +21,9 @@ public class KadaneAlgo {
 	public static void main(String[] args) {
 		int[] a = { -2, -3, 4, -1, -2, 1, 5, -3 };
 		System.out.println("Maximum contiguous sum is " + findLargestContiguousSubArray(a));
+
+		int a1[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+		System.out.println("Maximum contiguous sum is " + largestSUBSumArray(a1));
 	}
 
 	static int findLargestContiguousSubArray(int[] arr) {
@@ -31,10 +34,34 @@ public class KadaneAlgo {
 			if (max_ending_here < 0) {
 				max_ending_here = 0;// 1/2/7/4
 			}
-			if (max_ending_here > max_so_far) {
-				max_so_far = max_ending_here;// 4/7
-			}
+			/*
+			 * if (max_ending_here > max_so_far) { max_so_far =
+			 * max_ending_here;// 4/7 }
+			 */
+
+			/*
+			 * Do not compare for all elements. Compare only when
+			 * max_ending_here > 0
+			 */
+			else if (max_so_far < max_ending_here)
+				max_so_far = max_ending_here;
 		}
 		return max_so_far;
+	}
+
+	// above program will not work for all negative numbers as its simply
+	// returns 0 if all no will negative
+	// here is the method to find if array value will be negative number
+
+	static int largestSUBSumArray(int[] arr) {
+		int max_so_far = arr[0];
+		int curr_max = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			// comparing values which is max
+			curr_max = Math.max(arr[i], curr_max + arr[i]);
+			max_so_far = Math.max(max_so_far, curr_max);
+		}
+		return max_so_far;
+
 	}
 }
