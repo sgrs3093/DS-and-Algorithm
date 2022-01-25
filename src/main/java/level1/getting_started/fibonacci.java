e9fcc279663e1deccbed19b038b2f3dcab4b2c0a
Sagar Shukla <sagar.shukla@sap.com>
2022-01-25 20:09:46 +0530

    pep coding level 1 is fibonacci series

---
+++ src/main/java/level1/getting_started/fibonacci.java
@@ -0,0 +1,28 @@
+package level1.getting_started;
+
+import java.util.Scanner;
+
+/**
+ * Print fibonacci till N
+ */
+public class fibonacci
+{
+    /*
+     *  N=  10
+        o/p =    0 1 1 2 3 5 8 13 21 34
+     */
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+    }
+}
