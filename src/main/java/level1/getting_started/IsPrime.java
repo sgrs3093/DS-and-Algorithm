cc29114234dd9b0e152a5cbae73c6d20c9389f36
Sagar Shukla <sagar.shukla@sap.com>
2022-01-25 20:05:03 +0530

    pep coding level 1 is prime optimization

--- src/main/java/level1/getting_started/IsPrime.java
+++ src/main/java/level1/getting_started/IsPrime.java
@@ -10,7 +10,7 @@ public class IsPrime
         int t = scanner.nextInt();
         for (int i = 0; i < t; i++) {
             int n = scanner.nextInt();
-
+            //rootN(n);
             int count = 0;
 
             for (int div = 1; div <= n; div++) {
@@ -25,6 +25,30 @@ public class IsPrime
                 System.out.println("Not Prime");
             }
 
+        }
+    }
+
+    /**
+     * for any no factor we need to check up to root of the N , and on that if any number will get devided
+     * then we need  not to check further , we can stop there and say factor has been happend and it is not prime
+     * number
+     *
+     * @param n
+     */
+    static void rootN (int n)
+    {
+        int count = 0;
+        for (int div = 2; div * div <= n; div++) {
+            if (n % div == 0) {
+                count++;
+                break;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Prime");
+        }
+        else {
+            System.out.println("Not Prime");
         }
     }
 }
