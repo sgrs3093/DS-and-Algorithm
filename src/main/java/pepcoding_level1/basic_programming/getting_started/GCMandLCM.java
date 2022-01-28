848a616d4b2723ff800d42ebf84639850e28ea58
Sagar Shukla <sagar.shukla@sap.com>
2022-01-29 00:29:21 +0530

    pep coding level 1 is GCMandLCM

--- src/main/java/pepcoding_level1/basic_programming/getting_started/GCMandLCM.java
+++ src/main/java/pepcoding_level1/basic_programming/getting_started/GCMandLCM.java
@@ -1,9 +1,25 @@
 package pepcoding_level1.basic_programming.getting_started;
 
+import java.util.Scanner;
+
 public class GCMandLCM
 {
     public static void main (String[] args)
     {
+        Scanner scanner=new Scanner(System.in);
+        int n1=scanner.nextInt();
+        int n2=scanner.nextInt();
 
+        int on1=n1;
+        int on2=n2;
+        while (n1%n2!=0) {
+
+            int rem =n1%n2;
+            n1=n2;n2=rem;
+        }
+        int gcd=n2;
+        int lcm=(on1*on2)/gcd;
+        System.out.println(gcd);
+        System.out.println(lcm);
     }
 }
