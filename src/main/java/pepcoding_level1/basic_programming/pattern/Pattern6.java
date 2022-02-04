5d3fb3119ff0d9c49bd10ea8fea08c3cfbae554a
Sagar Shukla <sagar.shukla@sap.com>
2022-02-04 23:58:54 +0530

    pep coding level 1 is Pattern6

---
+++ src/main/java/pepcoding_level1/basic_programming/pattern/Pattern6.java
@@ -0,0 +1,34 @@
+package pepcoding_level1.basic_programming.pattern;
+
+import java.util.Scanner;
+
+public class Pattern6
+{
+
+    public static int getInput ()
+    {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+
+    public static void main (String[] args)
+    {
+        printPattern();
+    }
+
+
+    /*
+       for number = 5
+               space  star
+               2       1
+               1       3
+               0       5
+               1       3
+               2       1
+        */
+    private static void printPattern ()
+    {
+
+    }
+}
