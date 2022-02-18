e2c1279f375f58b02b531269a4a417d605e497c0
Sagar Shukla <sagar.shukla@sap.com>
2022-02-18 23:49:29 +0530

    pep coding level 1 is AnyBaseToDecimal

---
+++ src/main/java/pepcoding_level1/basic_programming/function_arrays/AnyBaseToDecimal.java
@@ -0,0 +1,38 @@
+package pepcoding_level1.basic_programming.function_arrays;
+
+import java.util.Scanner;
+
+/**
+ * Any Base To Decimal
+ * <p>
+ * 1. You are given a number n.
+ * 2. You are given a base b. n is a number on base b.
+ * 3. You are required to convert the number n into its corresponding value in decimal number system.
+ * <p>
+ * Sample Input:
+ * 111001
+ * 2
+ * Sample Output:
+ * 57
+ */
+public class AnyBaseToDecimal
+{
+
+    public static int getInput ()
+    {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static void main (String[] args)
+    {
+        anybasetoDecimal();
+    }
+
+    private static void anybasetoDecimal ()
+    {
+        System.out.println("Enter the number you want to convert");
+        int num = getInput();
+        System.out.println("Enter the base in which you want to convert");
+    }
+}
