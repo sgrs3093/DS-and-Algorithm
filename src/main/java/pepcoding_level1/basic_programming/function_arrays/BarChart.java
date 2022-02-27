204dff5b4d8f3644c445446523329de22e4c0e74
Sagar Shukla <sagar.shukla@sap.com>
2022-02-27 23:08:28 +0530

    pep coding level 1 is BarChart

---
+++ src/main/java/pepcoding_level1/basic_programming/function_arrays/BarChart.java
@@ -0,0 +1,5 @@
+package pepcoding_level1.basic_programming.function_arrays;
+
+public class BarChart
+{
+}
--- src/main/java/pepcoding_level1/basic_programming/function_arrays/FindElementInArray.java
+++ src/main/java/pepcoding_level1/basic_programming/function_arrays/FindElementInArray.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 /**
  * FindElement in Array
- *
+ * <p>
  * Input
  * A number n
  * n1
  * n2
  * .. n number of elements
  * A number d
- *
+ * <p>
  * Output
  * A number representing index at which d is found in array a and -1 if not found
  * Example
  * Sample Input
- *
+ * <p>
  * 6
- *
+ * <p>
  * 15
  * 30
  * 40
  * 4
  * 11
  * 9
- *
+ * <p>
  * 40
- *
+ * <p>
  * Sample Output
  * 2 ( we need to find the 40 , at which index it is present ) so out put index 2
  */
@@ -45,7 +45,29 @@ public class FindElementInArray
         findElement();
     }
 
-    static void findElement() {
+    static void findElement ()
+    {
 
+        /*
+        getting input
+
+         */
+        int n = getInput();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = getInput();
+        }
+        int ele = getInput();
+
+        // now we need to find the element
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == ele) {
+                //printing the index for corresponding element
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(-1);
+        return;
     }
 }
