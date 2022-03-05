d554c69577e64060926d9382dc0d245ad297a1ef
Sagar Shukla <sagar.shukla@sap.com>
2022-03-06 00:02:44 +0530

    pep coding level 1 is ReverseArray

--- src/main/java/pepcoding_level1/basic_programming/function_arrays/ReverseArray.java
+++ src/main/java/pepcoding_level1/basic_programming/function_arrays/ReverseArray.java
@@ -26,10 +26,38 @@ public class ReverseArray
 
     private static void display (int[] a)
     {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 
     private static void reverse (int[] a)
     {
+        int[] b = new int[a.length];
+        //here we need to reverse the array
+        int length = a.length - 1;// if length is 5 then 5-1 = 4 i.e 0 to 4 index
+        int j = 0;
+        // one way is to take one more array and reverse the existing array and store in B array
+        for (int i = length; i >= 0; i--) {
+            System.out.println(a[i]);
+            b[j++] = a[i];
+        }
+
+        //Printing B array
+        for (int i = 0; i < b.length; i++) {
+            System.out.println("b array :" + b[i]);
+        }
+
+        // second way is to in same array without taking extra space in same array we can reverse it
+        int x = 0;
+        int y = a.length - 1;
+        while (x < y) {
+            int temp = a[x];
+            a[x] = a[y];
+            a[y] = temp;
+            x++;
+            y--;
+        }
     }
 
 }
