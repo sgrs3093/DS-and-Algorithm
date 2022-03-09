813247595e0d80f480c87b3c0fc1f1fb52a7bd1f
Sagar Shukla <sagar.shukla@sap.com>
2022-03-09 23:14:33 +0530

    pep coding level 1 is SubArray

--- src/main/java/pepcoding_level1/basic_programming/function_arrays/SubArray.java
+++ src/main/java/pepcoding_level1/basic_programming/function_arrays/SubArray.java
@@ -3,6 +3,21 @@ package pepcoding_level1.basic_programming.function_arrays;
 /**
  * Sub array of Array
  * Link : https://www.pepcoding.com/resources/online-java-foundation/function-and-arrays/sub_arrays_of_an_array/topic
+ *
+ * Sample Input
+ * 3
+ * 10
+ * 20
+ * 30
+ *
+ * Sample Output
+ * 10
+ * 10	20
+ * 10	20	30
+ * 20
+ * 20	30
+ * 30
+ *
  */
 public class SubArray
 {
