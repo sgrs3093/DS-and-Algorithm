8f21e67fea31138c9875fe17ee276d796002a890
Sagar Shukla <sagar.shukla@sap.com>
2022-03-11 23:28:32 +0530

    pep coding level 1 is BrokenEconomy

---
+++ src/main/java/pepcoding_level1/basic_programming/function_arrays/BrokenEconomy.java
@@ -0,0 +1,28 @@
+package pepcoding_level1.basic_programming.function_arrays;
+
+/**
+ * Broken Economy
+ *
+ * Link :
+ * https://www.pepcoding.com/resources/online-java-foundation/function-and-arrays/broken_economy/topic
+ *
+ * Input Format
+ * A number n
+ * n1
+ * n2
+ * .. n number of elements
+ * A number d
+ *
+ * Output Format
+ * A number representing ceil
+ * A number representing floor
+ *
+ */
+public class BrokenEconomy
+{
+
+    public static void main (String[] args)
+    {
+
+    }
+}
