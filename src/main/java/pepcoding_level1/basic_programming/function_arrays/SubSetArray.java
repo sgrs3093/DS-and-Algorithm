c7679c434671dbcae0d81e3b09bf7ae0f3392b37
Sagar Shukla <sagar.shukla@sap.com>
2022-03-17 00:58:37 +0530

    pep coding level 1 is SubSetArray

--- src/main/java/pepcoding_level1/basic_programming/function_arrays/SubSetArray.java
+++ src/main/java/pepcoding_level1/basic_programming/function_arrays/SubSetArray.java
@@ -1,5 +1,7 @@
 package pepcoding_level1.basic_programming.function_arrays;
 
+import java.util.Scanner;
+
 /**
  * SubSet Of Array
  * Link :
@@ -31,6 +33,38 @@ public class SubSetArray
     public static void main (String[] args)
     {
         //taking input
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextInt();
+        }
+        int limit = (int)Math.pow(2,
+                                  arr.length);
+
+        for (int i = 0; i < limit; i++) {
+            //convert i to binary and use 0's and 1's to know if to print the element or not
+
+            String subset = "";
+            int temp = i;
+            for (int j = arr.length - 1; j >= 0; j--) {
+
+
+                int rem = temp % 2;
+                temp = temp / 2;
+
+                //then we dont need to print the element else we need to print the element
+                if (rem == 0) {
+                    subset =    "-\t" +subset;
+                }
+                else {
+                    subset =  arr[j]+  "-\t" +subset;
+                }
+            }
+            System.out.println(subset);
+        }
 
     }
 }
