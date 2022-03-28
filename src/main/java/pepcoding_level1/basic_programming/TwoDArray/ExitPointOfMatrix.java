34101407fe6b993e4ee37e5a5d31d4d91f70b685
Sagar Shukla <sagar.shukla@sap.com>
2022-03-28 23:48:41 +0530

    pep coding level 1 is ExitPointOfMatrix

---
+++ src/main/java/pepcoding_level1/basic_programming/TwoDArray/ExitPointOfMatrix.java
@@ -0,0 +1,18 @@
+package pepcoding_level1.basic_programming.TwoDArray;
+
+import java.util.Scanner;
+
+/**
+ * Exit point of matrix
+ * Link :
+ * https://www.pepcoding.com/resources/online-java-foundation/2d-arrays/exit_point_of_a_matrix/topic
+ */
+public class ExitPointOfMatrix
+{
+    public static void main (String[] args)
+    {
+
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
