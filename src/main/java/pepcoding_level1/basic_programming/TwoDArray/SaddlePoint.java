8a21c99389a22a58945855b1850367112b904a4e
Sagar Shukla <sagar.shukla@sap.com>
2022-04-13 22:58:30 +0530

    pep coding level 1 is SaddlePoint

--- src/main/java/pepcoding_level1/basic_programming/TwoDArray/SaddlePoint.java
+++ src/main/java/pepcoding_level1/basic_programming/TwoDArray/SaddlePoint.java
@@ -1,13 +1,30 @@
 package pepcoding_level1.basic_programming.TwoDArray;
 
+import java.util.Scanner;
+
 /**
  * Saddle Point
  * Link : https://www.pepcoding.com/resources/online-java-foundation/2d-arrays/saddle_price/topic
+ *
+ *
+ 3. The saddle price is defined as the least price in the row but the maximum price in the column of the matrix.
+ In ROW minimum and
+ In Column Maximum
+
  */
 public class SaddlePoint
 {
     public static void main (String[] args)
     {
+        Scanner scanner=new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int [][]arr =new int[n][n];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                arr[i][j]=scanner.nextInt();
+            }
+        }
 
     }
 }
