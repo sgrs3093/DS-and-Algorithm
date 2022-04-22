328a95c40825564543fe54e00d2ddc3e42276a8a
Sagar Shukla <sagar.shukla@sap.com>
2022-04-22 11:52:33 +0530

    pep coding level 1 is StringCompression

---
+++ src/main/java/pepcoding_level1/basic_programming/string_stringbuilder_arraylist/StringCompression.java
@@ -0,0 +1,15 @@
+package pepcoding_level1.basic_programming.string_stringbuilder_arraylist;
+
+/**
+ * Problem Name : String Compression
+ * <p>
+ * Sample Input
+ * wwwwaaadexxxxxx
+ * <p>
+ * Sample Output
+ * 1st Compression : wadex
+ * 2nd Compression w4a3dex6
+ */
+public class StringCompression
+{
+}
