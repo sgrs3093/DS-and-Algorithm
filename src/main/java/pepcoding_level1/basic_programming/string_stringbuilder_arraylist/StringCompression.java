97be2a9d2af8d071ec6281ff8ad67b4ebb3a5532
Sagar Shukla <sagar.shukla@sap.com>
2022-04-22 11:56:14 +0530

    pep coding level 1 is StringCompression

--- src/main/java/pepcoding_level1/basic_programming/string_stringbuilder_arraylist/StringCompression.java
+++ src/main/java/pepcoding_level1/basic_programming/string_stringbuilder_arraylist/StringCompression.java
@@ -1,5 +1,7 @@
 package pepcoding_level1.basic_programming.string_stringbuilder_arraylist;
 
+import java.util.Scanner;
+
 /**
  * Problem Name : String Compression
  * <p>
@@ -12,5 +14,21 @@ package pepcoding_level1.basic_programming.string_stringbuilder_arraylist;
  */
 public class StringCompression
 {
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String value = sc.next();
+        firstCompression(value);
+        secondCompression(value);
+    }
 
+    static void firstCompression (String value)
+    {
+
+    }
+
+    static void secondCompression (String value)
+    {
+
+    }
 }
