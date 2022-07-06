ef8fe2353b2af0d994749a80ef166133e5e0f9e2
sagar <sagar.shukla@sap.com>
2022-07-06 07:11:41 +0530

    stack intro

---
+++ src/main/java/pepcoding_level1/basic_programming/stackQueues/DuplicateBracket.java
@@ -0,0 +1,22 @@
+package pepcoding_level1.basic_programming.stackQueues;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * https://www.pepcoding.com/resources/online-java-foundation/stacks-and-queues/duplicate_brackets/topic
+ */
+public class DuplicateBracket {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+
+        }
+
+    }
+}
